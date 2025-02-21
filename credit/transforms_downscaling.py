45c1113a9d81e5fa3884d52d6201d893124fcf1e
Seth McGinnis <mcginnis@ucar.edu>
2025-02-20 17:50:45 -0700

    cleanup

--- credit/transforms_downscaling.py
+++ credit/transforms_downscaling.py
@@ -29,18 +29,11 @@ class Expand:
             n = len(x.shape)
             return x.repeat(selfby, axis=n-1).repeat(self,by, axis=n-2)
         
-# def expand(x, by, inverse=False):
-#         if inverse:
-#             return x[...,::by,::by]
-#         else:
-#             n = len(x.shape)
-#             return x.repeat(by, axis=n-1).repeat(by, axis=n-2)
 
-
-## Note: we don't want sklearn functions becaue they calcluate params
-## from the data, and we want to use externally-specified param
-## values.  We also need an inverse for each function.
-
+## Note: we don't want sklearn functions for normalization because
+## they calcluate params from the data, and we want to use
+## externally-specified param values.  We also need an inverse for
+## each function.
 
 def rescale(x, offset=0, scale=1, inverse=False):
     if inverse:
@@ -55,12 +48,7 @@ class Minmax:
     inverse: bool = False
 
     def __call__(self, x):
-        rescale(x, self.mmin, self.mmax-self.mmin, self.inverse)
-
-        
-# def minmax(x, *, mmin, mmax, inverse=False):
-#     return rescale(x, mmin, mmax-mmin, inverse)
-
+        return rescale(x, self.mmin, self.mmax-self.mmin, self.inverse)
 
 @dataclass
 class Zscore:
@@ -69,13 +57,9 @@ class Zscore:
     inverse: bool = False
 
     def __call(self, x):
-        rescale(x, self.mean, self.stdev, self.inverse)
-    
-#def zscore(x, *, mean, stdev, inverse=False):
-#    return rescale(x, mean, stdev, inverse)
+        return rescale(x, self.mean, self.stdev, self.inverse)
 
-
-## rename argument 'pow' - nameclash with function
+## TODO: rename argument 'pow' - nameclash with function
 @dataclass
 class Power:
     pow: float
@@ -86,19 +70,10 @@ class Power:
             return np.power(x, 1/self.pow)
         else:
             return np.power(x, self.pow)
-    
-# def power(x, *, pow, inverse=False):
-#     if inverse:
-#         return np.power(x, 1/pow)
-#     else:
-#         return np.power(x, pow)
 
     
-## Inverse is the same as forward.  (If I didn't want precip < 0 on
-## input, I also don't want it on output.)
-
-#def clip(x, cmin=None, cmax=None, inverse=False):
-#    return np.clip(x, cmin, cmax)
+## Inverse for clipping is the same as forward.  (If I didn't want
+## precip < 0 on input, I also don't want it on output.)
 
 @dataclass
 class Clip:
@@ -108,15 +83,13 @@ class Clip:
     def __call__(self, x):
         return np.clip(x, a_min=self.cmin, a_max=self.cmax)
 
+    
 ## the 'do-nothing' op
 @dataclass
 class Identity:
     inverse: bool=False
     def __call__(self, x):
         return x
-
-#def identity(x, inverse=False, **kwargs):
-#    return x
 
 #..# @Dataclass
 #..# class Composition:
@@ -131,46 +104,6 @@ class Identity:
 #..#             # call trasnforms on x in order
 #..#             pass
 #..# 
-
-#@dataclass
-#class DataTransform:
-#    fname: str
-#    params: dict=None
-#    inverse: bool=False
-#
-#    ## map names to classes using dict to avoid insecure exec/eval
-#    classdict: ClassVar[dict] = {"expand": Expand,
-#                                 "minmax": Minmax,
-#                                 "zscore": Zscore,
-#                                 "power": Power,
-#                                 "clip": Clip,
-#                                 "identity": Identity,
-#                                 None: Identity,  # do nothing if no transform defined
-#                               }
-#    
-#    def __call__(self, x):
-#        return fundict[self.fname](x, **self.params, inverse=self.inverse)
-
-
-## creates a collection of DataTransforms for a DataMap. 
-    
-##  Need a class that reads netcdf & instantiates all datatransforms
-##  based on conf; then, given a dict of ndrrays, it applies all the
-##  transforms to the relevant variables.  Invertible
-
-
-
-#
-#
-#classdict: ClassVar[dict] = {"expand": Expand,
-#                             "minmax": Minmax,
-#                             "zscore": Zscore,
-#                             "power": Power,
-#                             "clip": Clip,
-#                             "identity": Identity,
-#                             None: Identity,  # do nothing if no transform defined
-#                             }
-#
 
 ## inversion needs to be a switch that you flip
 
@@ -207,7 +140,7 @@ class DownscalingNormalizer:
                 pfile = nc.Dataset(rootpath + "/" + transdict['paramfiles'][par])
                 for var in variables:
                     if var in pfile.variables:
-                        params[par][var] = pfile.variables[var][...].item()
+                        params[par][var] = pfile.variables[var][...]  #.item()
 
         ## add explicitly specified parameter values, possibly overriding netcdf
         for var in variables:
@@ -225,7 +158,7 @@ class DownscalingNormalizer:
             if var in transdict or 'default' in transdict:
                 xkey = var if var in transdict else 'default'
                 for xform in transdict[xkey]:
-                    x = xclassdict[xform]
+                    x = self.xclassdict[xform]
                     xparams = {}
                     for arg in inspect.signature(x).parameters.keys():
                         if var in params[arg]:
@@ -233,10 +166,15 @@ class DownscalingNormalizer:
                     # instantiate new transform & append to list                    
                     self.transforms[var].append(x(**xparams))
             else:
-                self.transforms[var].append(xclassdict[None]())
+                self.transforms[var].append(self.xclassdict[None]())
             
     
     def __call__(self, x):
+        # x is a nested dict of [usage][var][time,(z),y,x]
+        for usage in x:
+            for var in x[usage]:
+                
+                pass
         # iterate over sample structure
         # call self.transforms[var] for each variable
         # (it's a list; iterate over it)
