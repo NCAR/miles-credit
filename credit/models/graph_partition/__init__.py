a64a3fb9a688c3a98a79ef435205774d73b30f22
Arnold Kazadi <arnoldnyamabo@gmail.com>
2025-07-08 13:31:31 -0600

    Removed unused import

--- credit/models/graph_partition/__init__.py
+++ credit/models/graph_partition/__init__.py
@@ -1,4 +1,4 @@
 from .autograd import all_gather_v, indexed_all_to_all_v
 from .graph_csc import CuGraphCSC
-from .graph_csc_pyg import CuGraphCSC as pygCuGraphCSC
+# from .graph_csc_pyg import CuGraphCSC as pygCuGraphCSC
 from .utils import get_lat_lon_partition_separators
