d0aa65481718bf53a458a98821160dd6a4ca0c15
Ben Kirk <benkirk@ucar.edu>
2025-09-19 08:49:46 -0600

    create Derecho CREDIT environment from a conda base including the Derecho-specific torch & MPI bits

--- create_derecho_env.sh
+++ create_derecho_env.sh
@@ -15,19 +15,13 @@ set -e
 # cd $CURR_DIR
 # pip install ${WHEEL_DIR}/torch-2.5.1+derecho.gcc.12.4.0.cray.mpich.8.1.29-cp311-cp311-linux_x86_64.whl
 # pip install ${WHEEL_DIR}/torchvision-0.20.1+derecho.gcc.12.4.0-cp311-cp311-linux_x86_64.whl
-#pip install -e .
+# pip install -e .
 
 ml conda
 
 topdir=$(git rev-parse --show-toplevel)
 CREDIT_ENV_PATH=${CREDIT_ENV_PATH:-"${topdir}/derecho-env"}
 
-
-# Notes on the choices below:
-#  install xgboost along with pytorch via conda.  This will then use a consistent NCCL.
-#  then when we install echo-opt via pip the xgboost dependency is already satisfied.
-#  otherwise, echo-opt -> xgboost -> nvidia-nccl-cu12 -> problem.
-export CONDA_OVERRIDE_CUDA="12.3"
 cat <<EOF > derecho.yml
 name: credit
 channels:
@@ -35,38 +29,12 @@ channels:
   - conda-forge
 dependencies:
   - python=3.11
-  - cartopy
   - conda-tree
-  - dask
-  - dask-jobqueue
-  - distributed
-  - einops
-  - fsspec
-  - gcsfs
-  - haversine
-  - jupyter
-  - keras
-  - matplotlib
-  - metpy
   - mpi4py =*=derecho*
-  - netcdf4
-  - numpy
-  - optuna==3.6.0
-  - pandas
   - pip
-  - pre-commit
-  - pvlib
-  - pyarrow
-  - pytest
   - pytorch ==2.5.1=derecho*
-  - pyyaml
-  - ruff
-  - scikit-learn
-  - segmentation-models-pytorch
-  - torchmetrics
   - torchvision =*=derecho*
-  - xarray
-  - zarr
+  - torchmetrics
   - pip:
     - pipdeptree
     - .
@@ -77,10 +45,10 @@ export CONDA_VERBOSITY=1
 export TIMEFORMAT=$'--> Real time: %3R seconds'
 time conda env create --prefix ${CREDIT_ENV_PATH} --file derecho.yml
 
-
 conda activate ${CREDIT_ENV_PATH}
 
-# remove PIP NCCL,if any
+# remove PIP NCCL, if any.
+#  (echo-opt -> xgboost -> nvidia-nccl-cu12 -> problem.)
 pip uninstall -y $(pip list | grep nvidia-nccl | awk '{print $1}')
 
 conda-tree deptree --small
