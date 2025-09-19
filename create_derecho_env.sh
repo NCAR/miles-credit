9646909e4e1d8c7c075d9811e783896f02c6a365
Ben Kirk <benkirk@ucar.edu>
2025-09-19 11:01:33 -0600

    temporary YAML file

--- create_derecho_env.sh
+++ create_derecho_env.sh
@@ -17,12 +17,21 @@ set -e
 # pip install ${WHEEL_DIR}/torchvision-0.20.1+derecho.gcc.12.4.0-cp311-cp311-linux_x86_64.whl
 # pip install -e .
 
+
+#-----------------------------------------------------------
+# set up an initial conda environment at ${CREDIT_ENV_PATH}
+# containing Derecho-specific torch & MPI bits.
+# (install torchmetrics at this point too, installing it later
+# via pip risks an undesirable torch update.)
 ml conda
 
 topdir=$(git rev-parse --show-toplevel)
 CREDIT_ENV_PATH=${CREDIT_ENV_PATH:-"${topdir}/derecho-env"}
+yml=$(mktemp --tmpdir=${topdir} credit-derecho-tmp-XXXXXXXXXX.yml)
+echo ${yml}
+#yml=derecho.yml
 
-cat <<EOF > derecho.yml
+cat <<EOF > ${yml}
 name: credit
 channels:
   - file:///glade/work/benkirk/consulting/conda-recipes/output
@@ -43,9 +52,17 @@ EOF
 # create the environment
 export CONDA_VERBOSITY=1
 export TIMEFORMAT=$'--> Real time: %3R seconds'
-time conda env create --prefix ${CREDIT_ENV_PATH} --file derecho.yml
+time conda env create --prefix ${CREDIT_ENV_PATH} --file ${yml} \
+    || { cat ${yml}; echo "ERROR Creating Conda env!"; exit 1; }
 
+rm -f ${yml}
+
+#-----------------------------------------------------------
+# activate & fix the environment
 conda activate ${CREDIT_ENV_PATH}
+
+echo "NCCLs - before cleanup:"
+find ${CONDA_PREFIX} -name "libnccl.*"
 
 # remove PIP NCCL, if any.
 #  (echo-opt -> xgboost -> nvidia-nccl-cu12 -> problem.)
@@ -54,6 +71,9 @@ pip uninstall -y $(pip list | grep nvidia-nccl | awk '{print $1}')
 conda-tree deptree --small
 pipdeptree --depth 3
 
+echo "NCCLs - after cleanup:"
 find ${CONDA_PREFIX} -name "libnccl.*"
 
 python -c "import credit"
+
+echo "CREDIT for Derecho successfully installed into ${CREDIT_ENV_PATH}"
