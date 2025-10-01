e276315c1dc75f906f48f46df851abc0564deb9d
John Schreck <jsschreck@gmail.com>
2025-10-01 11:45:32 -0600

    Added missing import

--- credit/postblock/__init__.py
+++ credit/postblock/__init__.py
@@ -1,2 +1,2 @@
-from credit.postblock._postblock import PostBlock, GlobalMassFixer, GlobalWaterFixer, GlobalEnergyFixer
+from credit.postblock._postblock import PostBlock, TracerFixer, GlobalMassFixer, GlobalWaterFixer, GlobalEnergyFixer
 from credit.postblock.wet_mask_samudra import WetMaskBlock
