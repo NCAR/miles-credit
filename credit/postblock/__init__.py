32a5df3a6f077b4ecb05da78b82ba2951e2bd587
John Schreck <jsschreck@gmail.com>
2025-10-01 11:30:29 -0600

    Fixing import error in postblock

--- credit/postblock/__init__.py
+++ credit/postblock/__init__.py
@@ -1,2 +1,2 @@
-from ._postblock import PostBlock, GlobalMassFixer, GlobalWaterFixer, GlobalEnergyFixer
-from .wet_mask_samudra import WetMaskBlock
+from credit.postblock._postblock import PostBlock, GlobalMassFixer, GlobalWaterFixer, GlobalEnergyFixer
+from credit.postblock.wet_mask_samudra import WetMaskBlock
