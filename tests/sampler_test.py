37510816f2a3cc83fe5a80044b5b6c5246900c93
Charlie Becker <31113893+charlie-becker@users.noreply.github.com>
2026-02-12 14:34:41 -0700

    Update sampler_test.py

--- tests/sampler_test.py
+++ tests/sampler_test.py
@@ -81,23 +81,26 @@ def minimal_config():
         "end_datetime": "2023-01-05",
         "source": {
             "ERA5": {
-                "prognostic": {
-                    "vars_3D": ["T", "U"],
-                    "vars_2D": ["SP"],
-                    "path": "/fake/*.zarr",
-                },
-                "dynamic_forcing": {
-                    "vars_2D": ["tsi"],
-                    "path": "/fake/*.zarr",
-                },
-                "static": {
-                    "vars_2D": ["LSM"],
-                    "path": "/fake/*.zarr",
-                },
-                "diagnostic": {
-                    "vars_2D": ["TP"],
-                    "path": "/fake/*.zarr",
-                },
+                "level_coord": "level",
+                "levels": [100, 85, 50, 30],
+                "variables": {
+                    "prognostic": {
+                        "vars_3D": ["T", "U"],
+                        "vars_2D": ["SP"],
+                        "path": "/fake/*.zarr",
+                    },
+                    "dynamic_forcing": {
+                        "vars_2D": ["tsi"],
+                        "path": "/fake/*.zarr",
+                    },
+                    "static": {
+                        "vars_2D": ["LSM"],
+                        "path": "/fake/*.zarr",
+                    },
+                    "diagnostic": {
+                        "vars_2D": ["TP"],
+                        "path": "/fake/*.zarr",
+                    }},
             }
         },
     }
