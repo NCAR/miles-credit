8f8ef0548f319f0120abaa449b9a3f470c86731e
David John Gagne <davidjohngagne@gmail.com>
2026-03-18 17:05:56 -0600

    Added ARCOERA5Dataset to init

--- credit/datasets/__init__.py
+++ credit/datasets/__init__.py
@@ -4,10 +4,10 @@ import glob
 import logging
 
 from credit.datasets.multi_source import MultiSourceDataset
-from credit.datasets.era5 import ERA5Dataset
+from credit.datasets.era5 import ERA5Dataset, ARCOERA5Dataset
 from credit.datasets.MRMS import MRMSDataset
 
-__all__ = ["MultiSourceDataset", "ERA5Dataset", "MRMSDataset"]
+__all__ = ["MultiSourceDataset", "ERA5Dataset", "MRMSDataset", "ARCOERA5Dataset"]
 
 
 logger = logging.getLogger(__name__)
