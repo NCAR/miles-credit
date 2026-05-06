9017838b4fc053d9ca2db3de5911d5e973d907bb
kevinyang-cky <kevinyang@ucar.edu>
2026-05-06 12:13:40 -0600

    include ARCOERA5Dataset

--- credit/datasets/multi_source.py
+++ credit/datasets/multi_source.py
@@ -46,6 +46,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 from credit.datasets.era5 import ERA5Dataset
+from credit.datasets.era5 import ARCOERA5Dataset
 from credit.datasets.MRMS import MRMSDataset
 from credit.datasets.goes import GOESDataset
 from credit.datasets.hrrr import HRRRDataset
@@ -56,6 +57,7 @@ logger = logging.getLogger(__name__)
 # Add entries here to register new data sources.
 _SOURCE_REGISTRY: dict[str, type] = {
     "ERA5": ERA5Dataset,
+    "ARCO_ERA5": ARCOERA5Dataset,
     "MRMS": MRMSDataset,
     "GOES": GOESDataset,
     "HRRR": HRRRDataset,
