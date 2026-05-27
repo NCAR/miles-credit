f158d3094054638a0d834b26fe3058d8c6f89691
kevinyang-cky <kevinyang@ucar.edu>
2026-05-27 14:29:30 -0600

    revert changes

--- scripts/multi_source_example.py
+++ scripts/multi_source_example.py
@@ -18,33 +18,33 @@ with open(config_path, "r") as f:
 nfs = config["data"]["forecast_len"]
 ms_dataset = MultiSourceDataset(config["data"], return_target=True)
 ms_sampler = DistributedMultiStepBatchSampler(
-    ms_dataset, batch_size=1, num_forecast_steps=nfs, shuffle=False, num_replicas=1, rank=0
+    ms_dataset, batch_size=4, num_forecast_steps=nfs, shuffle=True, num_replicas=1, rank=0
 )
 ms_loader = DataLoader(ms_dataset, batch_sampler=ms_sampler, num_workers=0, pin_memory=False, prefetch_factor=None)
 sample = next(iter(ms_loader))
-#print(sample.keys())
-#print(sample["input"].keys())
+print(sample.keys())
+print(sample["input"].keys())
 print(sample)
-# print(sample["input"]["ERA5_UpperAir"].keys())
-# preblocks = build_preblocks(config["preblocks"])
-# batch = apply_preblocks(preblocks, sample)
-# print("BATCH KEYS:", batch.keys())
-# print("Input shape:", batch["input"].shape)
-# print("Target shape:", batch["target"].shape)
-# target_shape = batch["target"].shape
+print(sample["input"]["ERA5_UpperAir"].keys())
+preblocks = build_preblocks(config["preblocks"])
+batch = apply_preblocks(preblocks, sample)
+print("BATCH KEYS:", batch.keys())
+print("Input shape:", batch["input"].shape)
+print("Target shape:", batch["target"].shape)
+target_shape = batch["target"].shape
 
-# print("METADATA KEYS:", batch["metadata"].keys())
-# print("METADATA KEYS:", batch["metadata"]["input"].keys())
-# print("METADATA KEYS:", batch["metadata"]["input"]["ERA5_LowerAir"].keys())
-# print("METADATA KEYS:", batch["metadata"]["input"]["ERA5_LowerAir"]["datetime"])
-# # print("METADATA CHANNEL_MAP KEYS:", batch['metadata']['target']['_channel_map']['ERA5_LowerAir/prognostic/3d/V'].keys())
-# # print("METADATA CHANNEL_MAP KEYS:", batch['metadata']['input']['_channel_map']['ERA5_LowerAir/prognostic/3d/V']['slice'])
+print("METADATA KEYS:", batch["metadata"].keys())
+print("METADATA KEYS:", batch["metadata"]["input"].keys())
+print("METADATA KEYS:", batch["metadata"]["input"]["ERA5_LowerAir"].keys())
+print("METADATA KEYS:", batch["metadata"]["input"]["ERA5_LowerAir"]["datetime"])
+# print("METADATA CHANNEL_MAP KEYS:", batch['metadata']['target']['_channel_map']['ERA5_LowerAir/prognostic/3d/V'].keys())
+# print("METADATA CHANNEL_MAP KEYS:", batch['metadata']['input']['_channel_map']['ERA5_LowerAir/prognostic/3d/V']['slice'])
 
-# fake_prediction = torch.tensor(np.random.normal(0, 1, target_shape))
-# batch["prediction"] = fake_prediction
-# sample["prediction"] = fake_prediction
+fake_prediction = torch.tensor(np.random.normal(0, 1, target_shape))
+batch["prediction"] = fake_prediction
+sample["prediction"] = fake_prediction
 
-# postblocks = build_postblocks(config["postblocks"])
-# postbatch = apply_postblocks(postblocks, batch)
-# print("POSTBATCH KEYS:", postbatch.keys())
-# print(postbatch["prediction"]["ERA5_UpperAir"].keys())
+postblocks = build_postblocks(config["postblocks"])
+postbatch = apply_postblocks(postblocks, batch)
+print("POSTBATCH KEYS:", postbatch.keys())
+print(postbatch["prediction"]["ERA5_UpperAir"].keys())
